4df3c3ea17e1a4de3b035a5691113ff99c1aa809
John Lamontagne <zanarchonline@yahoo.com>
2013-10-03 11:41:51 -0400

    Update Rect.cs
    Encapsulated the Rect class to prevent the setting of properties after class deceleration.

--- src/CEngineSharp Server/Utilities/Rect.cs
+++ src/CEngineSharp Server/Utilities/Rect.cs
@@ -8,17 +8,38 @@ namespace CEngineSharp_Server.Utilities
 {
     public class Rect
     {
+        private int _left;
+        private int _top;
+        private int _height;
+        private int _width;
+        
+        public int Left
+        {
+            get { return _left; }
+        }
+        
+        public int Top
+        {
+            get { return _top; }
+        }
+        
+        public int Height
+        {
+            get { return _height; }
+        }
+        
+        public int Width
+        {
+            get { return _width; }
+        }
+        
         public Rect(int left, int top, int height, int width)
         {
-            this.Left = left;
-            this.Top = top;
-            this.Height = height;
-            this.Width = width;
+            _left = left;
+            _top = top;
+            _height = height;
+            _width = width;
         }
 
-        public int Left;
-        public int Top;
-        public int Height;
-        public int Width;
     }
 }
